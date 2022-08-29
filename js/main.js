20900b9192bfeb51d182b80560c1415373e2268e
Yale Alves <yalealves1999@gmail.com>
2022-08-29 09:53:56 -0300

    Limpeza no código

--- js/main.js
+++ js/main.js
@@ -7,14 +7,11 @@ function startLoad(){
 function finalLoad(){
 	document.getElementById('loader').style.display = 'none';
 	document.getElementById('movies').style.display = 'block';
-
 }
 function searchTitles(e){
-	var titleSearch = document.getElementById('search').value;
-	var countrySearch = document.getElementById('country').value;
-	console.log(countrySearch);
+	let titleSearch = document.getElementById('search').value;
+	let countrySearch = document.getElementById('country').value;
 	findTitles(titleSearch, countrySearch);
-
 	e.preventDefault();
 }
 function findTitles(title, country){
@@ -22,24 +19,25 @@ function findTitles(title, country){
 	axios.get('https://apis.justwatch.com/content/titles/'+ country +'/popular?body={"content_types":["show","movie"],"providers":["nfx"],"page":1,"page_size":10,"query":"'+ title +'"}')
 
   .then(function (response) {
-    console.log(response);
-	var responseTitles = response.data.items;
-	var html = '';
-	for (var i = 0; i < responseTitles.length; i++){
-		var responseOffers = responseTitles[i].offers;
-		for (var j= 0; j < responseOffers.length; j++){
+	let responseTitles = response.data.items;
+	let html = '';
+	let netflixLink = '';
+	for (let i = 0; i < responseTitles.length; i++){
+		let responseOffers = responseTitles[i].offers;
+		for (let j= 0; j < responseOffers.length; j++){
 			if (responseOffers[j].provider_id == 8){
-				var netflix_link = responseOffers[j].urls.standard_web;
+				netflixLink = responseOffers[j].urls.standard_web;
 				break;
 			}
 		}
 
 		html += `
-		<a href="${netflix_link}" target="_blank"><h4>${responseTitles[i].title}</h4></a><br>
+		<a href="${netflixLink}" target="_blank"><h4>${responseTitles[i].title}</h4></a><br>
 
 		`;
 	}
-	if (responseTitles == ''){
+
+	if (!responseTitles.length){
 		html = '<h5>This title does not exist on netflix :(</h5>';
 	}
 	document.getElementById('movies').innerHTML = html;
